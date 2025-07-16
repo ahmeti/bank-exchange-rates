bd8842f6dd8da87935d926e45558f7af5ad11a01
Ahmet Imamoglu <ahmet363@gmail.com>
2025-07-16 21:01:40 +0300

    Garanti DATA_URL güncellendi.

--- src/Garanti.php
+++ src/Garanti.php
@@ -10,7 +10,7 @@ class Garanti
     const KEY = 'garanti';
     const NAME = 'Garanti';
     const BASE_URL = 'https://www.garantibbva.com.tr';
-    const DATA_URL = 'https://customers.garantibbva.com.tr/internet/digitalpublic/currency-convertor-public/v1/currency-convertor/currency-list-detail';
+    const DATA_URL = 'https://customers.garantibbva.com.tr/digital-public/currency-convertor-public/v2/currency-convertor/currency-list-detail';
     const REPLACES = [
         '/TL' => '/TRY',
         'TL/' => 'TRY/',
@@ -25,16 +25,31 @@ class Garanti
         $client = new Client();
         $headers = [
             'Accept' => 'application/json',
-            'Accept-Language' => 'tr-TR,tr;q=0.9',
+            'Accept-Language' => 'en,tr;q=0.9,en-US;q=0.8,ru;q=0.7',
+            'Cache-Control' => 'no-cache',
             'Connection' => 'keep-alive',
             'Content-Type' => 'application/json',
-            'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-            'channel' => 'Public',
+            'Origin' => 'https://webforms.garantibbva.com.tr',
+            'Pragma' => 'no-cache',
+            'Referer' => 'https://webforms.garantibbva.com.tr/',
+            'Sec-Fetch-Dest' => 'empty',
+            'Sec-Fetch-Mode' => 'cors',
+            'Sec-Fetch-Site' => 'same-site',
+            'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36',
+            'channel' => 'Internet',
+            'client-id' => 'DslahJXaDW59ibNZppCm',
+            'client-session-id' => 'c8fe-5015-c4f4-470e-8d91',
             'client-type' => 'ArkClient',
             'dialect' => 'TR',
             'guid' => str_replace('-', '', Uuid::uuid4()),
             'ip' => '127.0.0.1',
+            'sec-ch-ua' => '"Not)A;Brand";v="8", "Chromium";v="138", "Google Chrome";v="138"',
+            'sec-ch-ua-mobile' => '?0',
+            'sec-ch-ua-platform' => '"macOS"',
+            'state' => '',
+            'tenant-app-id' => '',
             'tenant-company-id' => 'GAR',
+            'tenant-geolocation' => 'TUR',
         ];
 
         $res = $client->get(self::DATA_URL, [
