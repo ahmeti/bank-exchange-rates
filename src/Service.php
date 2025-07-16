7e4aadbbe8cbccfb65fca3540b1d3fd743c1d000
Ahmet Imamoglu <ahmet363@gmail.com>
2025-07-16 21:59:53 +0300

    Service için try catch internal olarak aktif edildi. hasError ve getErrors metotları eklendi.

--- src/Service.php
+++ src/Service.php
@@ -3,15 +3,18 @@
 namespace Ahmeti\BankExchangeRates;
 
 use DateTimeZone;
+use Exception;
 
 class Service
 {
-    protected $rates = [];
+    protected array $rates = [];
+
+    protected array $exceptions = [];
 
     protected function merge(array $items): void
     {
         foreach ($items as $item) {
-            if (!array_key_exists($item['symbol'], $this->rates)) {
+            if (! array_key_exists($item['symbol'], $this->rates)) {
                 $this->rates[$item['symbol']] = [];
             }
 
@@ -26,7 +29,7 @@ class Service
 
     public static function toFloat(string $text): float
     {
-        return (float)str_replace(['.', ','], ['', '.'], $text);
+        return (float) str_replace(['.', ','], ['', '.'], $text);
     }
 
     public static function replace(array $replaces, $symbol): string
@@ -34,17 +37,71 @@ class Service
         return str_replace(array_keys($replaces), array_values($replaces), $symbol);
     }
 
+    public function hasError(): bool
+    {
+        return ! empty($this->exceptions);
+    }
+
+    public function getErrors(): array
+    {
+        return $this->exceptions;
+    }
+
     public function get(): array
     {
-        $this->merge((new Garanti)->get());
-        $this->merge((new YapiKredi)->get());
-        $this->merge((new HalkBank)->get());
-        $this->merge((new EnPara)->get());
-        $this->merge((new AkBank)->get());
-        $this->merge((new IsBankasi)->get());
-        $this->merge((new KuveytTurk)->get());
-        $this->merge((new Ziraat)->get());
-        $this->merge((new CepteTeb)->get());
+        try {
+            $this->merge((new Garanti)->get());
+        } catch (Exception $exception) {
+            $this->exceptions[Garanti::KEY] = $exception;
+        }
+
+        try {
+            $this->merge((new YapiKredi)->get());
+        } catch (Exception $exception) {
+            $this->exceptions[YapiKredi::KEY] = $exception;
+        }
+
+        try {
+            $this->merge((new HalkBank)->get());
+        } catch (Exception $exception) {
+            $this->exceptions[HalkBank::KEY] = $exception;
+        }
+
+        try {
+            $this->merge((new EnPara)->get());
+        } catch (Exception $exception) {
+            $this->exceptions[EnPara::KEY] = $exception;
+        }
+
+        try {
+            $this->merge((new AkBank)->get());
+        } catch (Exception $exception) {
+            $this->exceptions[AkBank::KEY] = $exception;
+        }
+
+        try {
+            $this->merge((new IsBankasi)->get());
+        } catch (Exception $exception) {
+            $this->exceptions[IsBankasi::KEY] = $exception;
+        }
+
+        try {
+            $this->merge((new KuveytTurk)->get());
+        } catch (Exception $exception) {
+            $this->exceptions[KuveytTurk::KEY] = $exception;
+        }
+
+        try {
+            $this->merge((new Ziraat)->get());
+        } catch (Exception $exception) {
+            $this->exceptions[Ziraat::KEY] = $exception;
+        }
+
+        try {
+            $this->merge((new CepteTeb)->get());
+        } catch (Exception $exception) {
+            $this->exceptions[CepteTeb::KEY] = $exception;
+        }
 
         return $this->rates;
     }
